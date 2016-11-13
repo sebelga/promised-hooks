727acd7df7ca5ebcb76faa678bcebd9a00811d91
sebelga <sabee77@gmail.com>
2016-11-13 14:14:16 +0100

    added eslint

--- hooks.js
+++ hooks.js
@@ -9,194 +9,176 @@ class HooksPromise {
         scope.pre = this.pre;
         scope.post = this.post;
         scope.hook = this.hook;
-        scope.__setupHooks = this.__setupHooks
-    };
+        scope.__setupHooks = this.__setupHooks;
+    }
 
     static hook(name, originalMethod) {
-            let proto = this;
-            // if (!proto.hasOwnProperty(name)) {
-            //     proto = this;
-            // }
+        const proto = this;
 
-            const pres = proto.__pres = proto.__pres || {};
-            const posts = proto.__posts = proto.__posts || {};
+        const presHooks = proto.__pres = proto.__pres || {};
+        const postsHooks = proto.__posts = proto.__posts || {};
 
-            pres[name] = pres[name] || [];
-            posts[name] = posts[name] || [];
+        presHooks[name] = presHooks[name] || [];
+        postsHooks[name] = postsHooks[name] || [];
 
-            // We wrapp the original method with he hooked logig
-            proto[name] = function() {
-                const passedArgs = Array.prototype.slice.apply(arguments);
-                const self = this;
+        // We wrapp the original method with he hooked logig
+        proto[name] = function wrapper() {
+            const passedArgs = Array.prototype.slice.apply(arguments);
+            const self = this;
+
+            /**
+             * Array or pre hooks
+             */
+            const pres = this.__pres[name] || this.prototype.__pres[name];
+
+            /**
+             * Array of post hooks
+             */
+            const posts = this.__posts[name] || this.prototype.__posts[name];
+
+            /**
+             * Total pre hooks
+             */
+            const totalPres = pres.length;
+
+            /**
+             * Total posts hooks
+             */
+            const totalPost = posts.length;
+
+            /**
+             * Scope for the hooks
+             */
+            let scope;
+
+            /**
+             * Current hook being processed
+             */
+            let currentPre = -1;
+
+            /**
+             * arguments eventually passed to the hook - are mutable
+             */
+            let hookArgs;
+
+            let resolveFn;
+            let rejectFn;
+
+            if (self.preHooksEnabled === false) {
+                return done.apply(self, passedArgs);
+            }
+
+            return new Promise((resolve, reject) => {
+                resolveFn = resolve;
+                rejectFn = reject;
 
                 /**
-                 * Array or pre hooks
+                 * Error Handler
                  */
-                const pres = this.__pres[name] || this.__proto__.__pres[name];
-
-                /**
-                 * Array of post hooks
-                 */
-                const posts = this.__posts[name] || this.__proto__.__posts[name];;
-
-                /**
-                 * Total pre hooks
-                 */
-                const totalPres = pres.length;
-
-                /**
-                 * Total posts hooks
-                 */
-                const totalPost = posts.length;
-
-                /**
-                 * Scope for the hooks
-                 */
-                let scope;
-
-                /**
-                 * Current hook being processed
-                 */
-                let currentPre = -1;
-
-                /**
-                 * arguments eventually passed to the hook - are mutable
-                 */
-                let hookArgs;
-
-                let resolveFn;
-                let rejectFn;
-
-                if (self.preHooksEnabled === false) {
-                    return done.apply(self, passedArgs);
-                }
-
-                return new Promise(function(resolve, reject) {
-
-                    resolveFn = resolve,
-                    rejectFn = reject;
-
-                    /**
-                     * Error Handler
-                     */
-                    const handleError = function(err) {
-                        reject(err);
-                    };
-
-                    /**
-                     * Middleware (hook) wrapper
-                     */
-                    const next = function () {
-                        let args = Array.prototype.slice.apply(arguments);
-
-                        if (is.object(args[0]) && {}.hasOwnProperty.call(args[0], '__override')) {
-                            args = arrify(args[0].__override);
-                        }
-
-                        // If there is a __scopeHook function on the object
-                        // we call it to get the scope wanted for the hook
-                        scope = getScope(self, name, args);
-
-                        /**
-                         * Reference to current pre hook
-                         */
-                        let currentHook;
-                        let preArgs;
-
-                        if (args && args.length && typeof args[0] !== 'undefined') {
-                            hookArgs = args;
-                        };
-
-                        if (++currentPre < totalPres) {
-                            currentHook = pres[currentPre]
-
-                            return currentHook.apply(scope, hookArgs).then(next, handleError);
-                        }
-
-                        // hookArgs.push(resolve);
-                        return done.apply(scope, hookArgs);
-                    }
-
-                    return next.apply(this, passedArgs);
-                });
-
-                function done () {
-                    const args = Array.prototype.slice.apply(arguments);
-                    // let callback;
-
-                    // if (args.length > 0) {
-                    //     const hasCallback = is.fn(args[args.length - 1]);
-
-                    //     if (hasCallback) {
-                    //         callback = args.pop();
-                    //     }
-                    // }
-
-                    resolveFn = resolveFn || Promise.resolve;
-                    rejectFn = rejectFn || Promise.reject;
-
-                    let currentPost = -1;
-
-                    let next = function (data) {
-                        if (++currentPost < totalPost && self.__hooksEnabled !== false) {
-                            /**
-                             * Reference to current post hook
-                             */
-                            let currPost = posts[currentPost];
-
-                            // Call next "post" hook
-                            return currPost.call(scope, data).then(next, (err) => {
-                                // we convert response to object
-                                if(!is.object(data)) {
-                                    data = {
-                                        result: data
-                                    };
-                                }
-
-                                // create errors Array
-                                data.errorsPostHook = data.errorsPostHook || [];
-                                data.errorsPostHook.push(err);
-
-                                return next(data);
-                            });
-                        } else {
-                            // Resolve... we're done! :)
-                            return resolveFn(data);
-
-                            // return resolve(data);
-                        }
-                    };
-
-                    // We execute the actual (original) method
-                    const response = originalMethod.apply(self, args);
-
-                    // If there are post hooks, we chain the response with the hook
-                    if (totalPost > 0) {
-                        return response.then(next, rejectFn);
-                    }
-
-                    // no "post" hook, we're done!
-                    if (response === true) {
-                        return response;
-                    }
-
-                    return response.then(resolveFn, rejectFn);
-
+                const handleError = function handleError(err) {
+                    reject(err);
                 };
 
-            };
+                /**
+                 * Middleware (hook) wrapper
+                 */
+                const next = function next() {
+                    let args = Array.prototype.slice.apply(arguments);
 
-            proto[name].__numAsyncPres = 0;
-            proto[name].__hooked = true;
+                    if (is.object(args[0]) && {}.hasOwnProperty.call(args[0], '__override')) {
+                        args = arrify(args[0].__override);
+                    }
 
-            return this;
+                    // If there is a __scopeHook function on the object
+                    // we call it to get the scope wanted for the hook
+                    scope = getScope(self, name, args);
+
+                    /**
+                     * Reference to current pre hook
+                     */
+                    let currentHook;
+
+                    if (args && args.length && typeof args[0] !== 'undefined') {
+                        hookArgs = args;
+                    }
+
+                    if (currentPre + 1 < totalPres) {
+                        currentPre += 1;
+                        currentHook = pres[currentPre];
+
+                        return currentHook.apply(scope, hookArgs).then(next, handleError);
+                    }
+
+                    // hookArgs.push(resolve);
+                    return done.apply(scope, hookArgs);
+                };
+
+                return next.apply(this, passedArgs);
+            });
+
+            function done() {
+                const args = Array.prototype.slice.apply(arguments);
+
+                resolveFn = resolveFn || Promise.resolve;
+                rejectFn = rejectFn || Promise.reject;
+
+                let currentPost = -1;
+
+                const next = function next(data) {
+                    if (currentPost + 1 < totalPost && self.__hooksEnabled !== false) {
+                        currentPost += 1;
+                        /**
+                         * Reference to current post hook
+                         */
+                        const currPost = posts[currentPost];
+
+                        // Call next "post" hook
+                        return currPost.call(scope, data).then(next, (err) => {
+                            // we convert response to object
+                            if (!is.object(data)) {
+                                data = {
+                                    result: data,
+                                };
+                            }
+
+                            // create errors Array
+                            data.errorsPostHook = data.errorsPostHook || [];
+                            data.errorsPostHook.push(err);
+
+                            return next(data);
+                        });
+                    }
+
+                    // Resolve... we're done! :)
+                    return resolveFn(data);
+                };
+
+                // We execute the actual (original) method
+                const response = originalMethod.apply(self, args);
+
+                // If there are post hooks, we chain the response with the hook
+                if (totalPost > 0) {
+                    return response.then(next, rejectFn);
+                }
+
+                // no "post" hook, we're done!
+                if (response === true) {
+                    return response;
+                }
+
+                return response.then(resolveFn, rejectFn);
+            }
+        };
+
+        proto[name].__numAsyncPres = 0;
+        proto[name].__hooked = true;
+
+        return this;
     }
 
     static pre(name, fn) {
-        let proto = this;
-        // if (!proto.hasOwnProperty(name)) {
-        //     proto = this;
-        // }
+        const proto = this;
+
         const pres = proto.__pres = proto.__pres || {};
 
         proto.__setupHooks(proto, name);
@@ -207,10 +189,8 @@ class HooksPromise {
     }
 
     static post(name, fn) {
-        let proto = this;
-        // if (!proto.hasOwnProperty(name)) {
-        //     proto = this;
-        // }
+        const proto = this;
+
         const posts = proto.__posts = proto.__posts || {};
 
         proto.__setupHooks(proto, name);
@@ -230,9 +210,9 @@ class HooksPromise {
 // Helpers
 function getScope(self, hookName, args) {
     return self.__scopeHook &&
-            typeof self.__scopeHook === 'function' &&
-            typeof self.__scopeHook(hookName, args) !== 'undefined' ?
-                self.__scopeHook(hookName, args) : self;
+        typeof self.__scopeHook === 'function' &&
+        typeof self.__scopeHook(hookName, args) !== 'undefined' ?
+        self.__scopeHook(hookName, args) : self;
 }
 
 module.exports = HooksPromise;
--- test/hooks.js
+++ test/hooks.js
@@ -1,32 +1,34 @@
 'use strict';
 
 const chai = require('chai');
-const expect = chai.expect;
 const sinon = require('sinon');
-require('sinon-as-promised');
-
 const hooks = require('../index');
 const mocks = require('../mocks/model.mock');
+
+const expect = chai.expect;
 const Model = mocks.MyClass;
 const Model2 = mocks.MyOtherClass;
+require('sinon-as-promised');
 
 describe('hooks-promise', () => {
     let model;
 
     describe('pre()', () => {
-        let spyPre1, spyPre2, spyOriginalMethod;
-        let error = {
-            code: 500
+        let spyPre1;
+        let spyPre2;
+        let spyOriginalMethod;
+        const error = {
+            code: 500,
         };
 
-        let spies = {
-            preHook1: function() {
+        const spies = {
+            preHook1() {
                 this.newValue = 123;
                 return Promise.resolve();
             },
-            preHook2: function() {
+            preHook2() {
                 return Promise.resolve();
-            }
+            },
         };
 
         beforeEach(() => {
@@ -48,58 +50,48 @@ describe('hooks-promise', () => {
             spyOriginalMethod.restore();
         });
 
-        it('should execute pre hooks in correct order', () => {
-            return model.save().then(() => {
-                sinon.assert.callOrder(spyPre1, spyPre2, spyOriginalMethod);
-            });
-        });
+        it('should execute pre hooks in correct order', () => model.save().then(() => {
+            sinon.assert.callOrder(spyPre1, spyPre2, spyOriginalMethod);
+        }));
 
-        it('should pass the correct scope to the hook', () => {
-            return model.save().then(() => {
-                expect(model.newValue).equal(123);
-            });
-        });
+        it('should pass the correct scope to the hook', () => model.save().then(() => {
+            expect(model.newValue).equal(123);
+        }));
 
         it('should catch errors in hooks and not call the original method', () => {
-            model.pre('save', function() {
-                return Promise.reject(error);
-            });
+            model.pre('save', () => Promise.reject(error));
 
             return model.save().catch((err) => {
                 expect(err).equal(error);
-                expect(spyOriginalMethod.called).be.false;
+                expect(spyOriginalMethod.called).equal(false);
             });
         });
 
         it('should pass parameter to originalMethod', () => {
             const args = ['abc', 123];
-            return model.save('abc', 123).then((argsPassed) => {
+            return model.save('abc', 123).then(() => {
                 expect(spyOriginalMethod.getCall(0).args).deep.equal(args);
             });
         });
 
         it('preHook resolve should override original parameter passed', () => {
-            model.pre('save', function() {
-                return Promise.resolve({__override: ['newParam1', 'newParam2']});
-            });
+            model.pre('save', () => Promise.resolve({ __override: ['newParam1', 'newParam2'] }));
             return model.save(123, 'abc').then(() => {
                 expect(spyOriginalMethod.getCall(0).args).deep.equal(['newParam1', 'newParam2']);
             });
         });
 
         it('preHook resolve should override original parameter passed (single)', () => {
-            model.pre('save', function() {
-                return Promise.resolve({__override: 'newParam1'});
-            });
+            model.pre('save', () => Promise.resolve({ __override: 'newParam1' }));
             return model.save(123, 'abc').then(() => {
                 expect(spyOriginalMethod.getCall(0).args).deep.equal(['newParam1']);
             });
         });
 
         it('should work with methods from an object', () => {
-            let myObject = {
-                save: function() { return Promise.resolve(); },
-                test1: function() {}
+            const myObject = {
+                save() { return Promise.resolve(); },
+                test1() { },
             };
 
             hooks.wrap(myObject);
@@ -115,29 +107,28 @@ describe('hooks-promise', () => {
     });
 
     describe('post()', () => {
-        let spyPost1, spyPost2, spyOriginalMethod;
-        let error = {
-            code: 500
-        };
+        let spyPost1;
+        let spyPost2;
+        let spyOriginalMethod;
 
-        let spies = {
-            postHook1: function(result) {
+        const spies = {
+            postHook1(result) {
                 this.newValue = 456;
                 return Promise.resolve(result);
             },
-            postHook2: function(result) {
+            postHook2(result) {
                 return Promise.resolve(result);
-            }
+            },
         };
 
         beforeEach(() => {
             model = new Model();
             hooks.wrap(model);
-            model.__proto__.__posts = {save: []};
+            // model.__posts = { save: [] };
 
             spyPost1 = sinon.spy(spies, 'postHook1');
             spyPost2 = sinon.spy(spies, 'postHook2');
-            spyOriginalMethod = sinon.spy(model.__proto__, 'save');
+            spyOriginalMethod = sinon.spy(model, 'save');
 
             model.post('save', spies.postHook1);
             model.post('save', spies.postHook2);
@@ -149,28 +140,20 @@ describe('hooks-promise', () => {
             spyOriginalMethod.restore();
         });
 
-        it('should execute posts hooks in correct order', () => {
-            return model.save().then(() => {
-                sinon.assert.callOrder(spyOriginalMethod, spyPost1, spyPost2);
-            });
-        });
+        it('should execute posts hooks in correct order', () => model.save().then(() => {
+            sinon.assert.callOrder(spyOriginalMethod, spyPost1, spyPost2);
+        }));
 
-        it('should pass the correct scope to the hook', () => {
-            return model.save().then(() => {
-                expect(model.newValue).equal(456);
-            });
-        });
+        it('should pass the correct scope to the hook', () => model.save().then(() => {
+            expect(model.newValue).equal(456);
+        }));
 
-        it('should pass resolve value from originalMethod response', () => {
-            return model.save().then((response) => {
-                expect(response).equal('1234');
-            });
-        });
+        it('should pass resolve value from originalMethod response', () => model.save().then((response) => {
+            expect(response).equal('1234');
+        }));
 
         it('should override original resolve in post hooks', () => {
-            model.post('save', function() {
-                return Promise.resolve('5678');
-            });
+            model.post('save', () => Promise.resolve('5678'));
 
             return model.save().then((response) => {
                 expect(response).equal('5678');
@@ -178,46 +161,38 @@ describe('hooks-promise', () => {
         });
 
         it('should not reject promise on error', () => {
-            model.post('save', function() {
-                return Promise.reject({ code:500 });
-            });
+            model.post('save', () => Promise.reject({ code: 500 }));
 
-            model.post('save', function(data) {
-                return Promise.resolve(data);
-            });
+            model.post('save', data => Promise.resolve(data));
 
             return model.save().then((response) => {
                 expect(response).deep.equal({
                     result: '1234',
-                    errorsPostHook: [{code: 500}]
+                    errorsPostHook: [{ code: 500 }],
                 });
             });
         });
 
         it('should not create object if response is already one', () => {
-            let model = new Model2();
+            model = new Model2();
             hooks.wrap(model);
 
-            model.post('save', function() {
-                return Promise.reject({ code:500 });
-            });
+            model.post('save', () => Promise.reject({ code: 500 }));
 
-            model.post('save', function(data) {
-                return Promise.resolve(data);
-            });
+            model.post('save', data => Promise.resolve(data));
 
             return model.save().then((response) => {
                 expect(response).deep.equal({
                     data: [1, 2, 3],
-                    errorsPostHook: [{code: 500}]
+                    errorsPostHook: [{ code: 500 }],
                 });
             });
         });
 
         it('should work with methods from an object', () => {
-            let myObject = {
-                save: function() { return Promise.resolve(); },
-                test1: function() {}
+            const myObject = {
+                save() { return Promise.resolve(); },
+                test1() { },
             };
 
             hooks.wrap(myObject);
